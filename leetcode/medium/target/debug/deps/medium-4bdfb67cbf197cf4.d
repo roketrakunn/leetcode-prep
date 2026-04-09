9247fcb9d3750d8561ed2af2de0d4d7ff4127d4e
Roket <okuhlexesibe0@gmail.com>
2026-04-09 09:55:46 +0200

    clean staged files

--- leetcode/medium/target/debug/deps/medium-4bdfb67cbf197cf4.d
+++ leetcode/medium/target/debug/deps/medium-4bdfb67cbf197cf4.d
@@ -1,8 +1,9 @@
-/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/medium-4bdfb67cbf197cf4.d: src/main.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
+/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/medium-4bdfb67cbf197cf4.d: src/main.rs src/max_num_of_ops2.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
 
-/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/libmedium-4bdfb67cbf197cf4.rmeta: src/main.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
+/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/libmedium-4bdfb67cbf197cf4.rmeta: src/main.rs src/max_num_of_ops2.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
 
 src/main.rs:
+src/max_num_of_ops2.rs:
 src/longest_arith_seq.rs:
 src/right_triangles.rs:
 src/sub_seqs.rs:
--- leetcode/medium/target/debug/deps/medium-d37c0d21380f55af.d
+++ leetcode/medium/target/debug/deps/medium-d37c0d21380f55af.d
@@ -1,8 +1,9 @@
-/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/medium-d37c0d21380f55af.d: src/main.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
+/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/medium-d37c0d21380f55af.d: src/main.rs src/max_num_of_ops2.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
 
-/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/libmedium-d37c0d21380f55af.rmeta: src/main.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
+/home/jobby/Projects/LeetcoddesPrep/leetcode/medium/target/debug/deps/libmedium-d37c0d21380f55af.rmeta: src/main.rs src/max_num_of_ops2.rs src/longest_arith_seq.rs src/right_triangles.rs src/sub_seqs.rs src/maxi_string_pairs.rs src/k_partitions.rs src/maxi_gold_profit.rs src/number_of_houses.rs src/maxi_square_fence.rs src/min_abs_diff.rs src/min_no_strs.rs
 
 src/main.rs:
+src/max_num_of_ops2.rs:
 src/longest_arith_seq.rs:
 src/right_triangles.rs:
 src/sub_seqs.rs:
